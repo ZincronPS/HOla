8dcdc0f802df57f20e5fb12a5611ea55dbe90862
Jayvardhan Patil <jayvardhanpatil7811@gmail.com>
2023-01-16 23:26:53 +0530

    First running iteration of neural network It just adds new neurons to the net, for now. But doesn't add bias neuron yet ..

---
+++ neural_network.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Neuron
+{
+};
+typedef vector<Neuron> Layer;
+
+class Net
+{
+public:
+    Net(const vector<unsigned> &topology);
+    void feedForward(const vector<double> &inputVals){};
+    void backProp(const vector<double> &targetVals){};
+    void getResults(vector<double> &resultVals) const {};
+
+private:
+    vector<Layer> m_layers; // m_layers[layerNum][neuronNum]
+};
+
+Net::Net(const vector<unsigned> &topology)
+{
+    unsigned numLayers = topology.size();
+
+    for (unsigned layerNum = 0; layerNum < numLayers; ++layerNum)
+    {
+        m_layers.push_back(Layer());
+
+        // we have made a new layer, now fill it with ith neurons and
+        // add a bias neuron to the layer
+
+        for (unsigned neuronNum = 0; neuronNum < topology[layerNum]; ++neuronNum)
+        {
+            m_layers.back().push_back(Neuron());
+
+            cout << "MADE A NEURON " << endl;
+        }
+    }
+}
+int main()
+{
+
+    vector<unsigned> topology;
+
+    topology.push_back(3);
+    topology.push_back(2);
+    topology.push_back(1);
+
+    Net myNet(topology);
+
+    vector<double> inputVals;
+    myNet.feedForward(inputVals);
+
+    vector<double> targetVals;
+    myNet.backProp(targetVals);
+
+    vector<double> resultVals;
+    myNet.getResults(resultVals);
+}
